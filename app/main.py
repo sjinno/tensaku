953fd16b1330acd09e59380c322efb496e734084
Shohei Jinno <shohei.jinno@gmail.com>
2024-11-21 09:41:26 -0800

    feat: integrate Mangum to enable FastAPI deployment on AWS Lambda

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.routes import router
+from mangum import Mangum
 
 app = FastAPI(title="Grammar Correction API")
 
 # Include routes
 app.include_router(router)
+
+# Add a Mangum adapter to handle AWS Lambda events
+handler = Mangum(app, TEXT_MIME_TYPES=["application/json"])
